bd3df2deca7f9ac8bc1dfdae3c2b07b3faf516d6
jdsaifi <jdsaifi729@gmail.com>
2025-11-04 17:38:37 +0530

    updated cors

--- src/utils/corsOptions.ts
+++ src/utils/corsOptions.ts
@@ -3,7 +3,7 @@ export const corsOptions = {
         'http://localhost:3060',
         'https://law.mindsharpner.com',
         'http://localhost:8080',
-        'https://aikb-api.mindsharpner.com',
+        'https://aikb.mindsharpner.com',
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // Specify allowed methods
